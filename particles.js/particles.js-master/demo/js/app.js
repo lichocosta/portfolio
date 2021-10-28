92b0881270720ae8f8dd2daa1d6439f86122efaf
Lisandro Costa <lisandrocostamartorel@gmail.com>
2021-10-27 21:41:37 -0300

    Mod con particlesJS

---
+++ particles.js/particles.js-master/demo/js/app.js
@@ -0,0 +1,122 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+
+var color = '#75A5B7';
+var maxParticles = 40;
+
+// ParticlesJS Config.
+particlesJS('particles-js', {
+  'particles': {
+    'number': {
+      'value': maxParticles,
+      'density': {
+        'enable': true,
+        'value_area': (maxParticles * 20) * 3
+      }
+    },
+    'color': {
+      'value': color
+    },
+    'shape': {
+      'type': 'circle',
+      'stroke': {
+        'width': 0,
+        'color': '#000000'
+      },
+      'polygon': {
+        'nb_sides': 5
+      },
+    },
+    'opacity': {
+      'value': 0.5,
+      'random': false,
+      'anim': {
+        'enable': false,
+        'speed': 1,
+        'opacity_min': 0.1,
+        'sync': false
+      }
+    },
+    'size': {
+      'value': 3,
+      'random': true,
+      'anim': {
+        'enable': false,
+        'speed': 40,
+        'size_min': 0.1,
+        'sync': false
+      }
+    },
+    'line_linked': {
+      'enable': true,
+      'distance': 150,
+      'color': color,
+      'opacity': 1,
+      'width': 1
+    },
+    'move': {
+      'enable': true,
+      'speed': 2,
+      'direction': 'none',
+      'random': false,
+      'straight': false,
+      'out_mode': 'out',
+      'bounce': false,
+      'attract': {
+        'enable': false,
+        'rotateX': 600,
+        'rotateY': 1200
+      }
+    }
+  },
+  'interactivity': {
+    'detect_on': 'canvas',
+    'events': {
+      'onhover': {
+        'enable': true,
+        'mode': 'grab'
+      },
+      'onclick': {
+        'enable': false,
+        'mode': 'push'
+      },
+      'resize': true
+    },
+    'modes': {
+      'grab': {
+        'distance': 140,
+        'line_linked': {
+          'opacity': 1
+        }
+      },
+      'bubble': {
+        'distance': 400,
+        'size': 40,
+        'duration': 2,
+        'opacity': 8,
+        'speed': 3
+      },
+      'repulse': {
+        'distance': 200,
+        'duration': 0.4
+      },
+      'push': {
+        'particles_nb': 4
+      },
+      'remove': {
+        'particles_nb': 2
+      }
+    }
+  },
+  'retina_detect': true
+});
