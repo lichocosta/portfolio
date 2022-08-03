41cb35de4708d417c126caffffbf1b9feb5552c0
lichocosta <lisandrocostamartorel@gmail.com>
2022-08-03 19:34:51 +0200

    particles top and repulse

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ particlesJS.load('particles-js', 'particles.json', function() {
 /* Otherwise just put the config content (json): */
 
 var color = '#75A5B7';
-var maxParticles = 50;
+var maxParticles = 40;
 
 // ParticlesJS Config.
 particlesJS('particles-js', {
@@ -66,8 +66,8 @@ particlesJS('particles-js', {
     },
     'move': {
       'enable': true,
-      'speed': 5,
-      'direction': 'none',
+      'speed': 4,
+      'direction': 'top',
       'random': false,
       'straight': false,
       'out_mode': 'out',
@@ -84,7 +84,7 @@ particlesJS('particles-js', {
     'events': {
       'onhover': {
         'enable': true,
-        'mode': 'grab'
+        'mode': 'repulse'
       },
       'onclick': {
         'enable': false,
@@ -120,3 +120,30 @@ particlesJS('particles-js', {
   },
   'retina_detect': true
 });
+
+/*
+particlesJS("particles-js", {
+  particles: {
+      number: { value: 139, density: { enable: true, value_area: 1893.9543399174545 } },
+      color: { value: "#ffffff" },
+      shape: { type: "circle", stroke: { width: 0, color: "#000000" }, polygon: { nb_sides: 5 }, image: { src: "img/github.svg", width: 100, height: 100 } },
+      opacity: { value: 0.5, random: false, anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false } },
+      size: { value: 3, random: true, anim: { enable: false, speed: 40, size_min: 0.1, sync: false } },
+      line_linked: { enable: true, distance: 150, color: "#ffffff", opacity: 0.4, width: 1 },
+      move: { enable: true, speed: 6, direction: "top", random: false, straight: false, out_mode: "out", bounce: false, attract: { enable: false, rotateX: 600, rotateY: 1200 } },
+  },
+  interactivity: {
+      detect_on: "canvas",
+      events: { onhover: { enable: true, mode: "repulse" }, onclick: { enable: true, mode: "push" }, resize: true },
+      modes: {
+          grab: { distance: 596.8767510414887, line_linked: { opacity: 1 } },
+          bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+          repulse: { distance: 471.00478313478015, duration: 0.4 },
+          push: { particles_nb: 4 },
+          remove: { particles_nb: 2 },
+      },
+  },
+  retina_detect: true,
+});
+var count_particles, stats, update;
+*/
